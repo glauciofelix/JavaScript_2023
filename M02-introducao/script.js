6adb6577ebf148f9c35f26372e7e26bf9e88a274
Glaucio Felix <glaucio.fn@gmail.com>
2023-08-06 22:54:35 -0300

    first

---
+++ M02-introducao/script.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual seu nome?");
+// console.log(nome);
+
+var idade = prompt("Qual sua idade?")
+console.log(idade)
+
+document.write("<h3>Bem vindo " + nome + "</h3>")
+document.write("<img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'>")
